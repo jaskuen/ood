c831927db7def3a3efedcc09e4190396afef87b6
jaskuen <tankionlinebotserver@mail.ru>
2025-10-02 20:06:34 +0300

    feat: program

--- Coffee/Cafe/Program.cs
+++ Coffee/Cafe/Program.cs
@@ -3,6 +3,7 @@
 using Cafe.Lib;
 using Cafe.Lib.Condiments;
 using Cafe.Lib.Implementation.CoffeeBeverages;
+using Cafe.Lib.Implementation.TeaBeverages;
 
 IBeverage coffee = new Latte(CoffeeSize.Standard);
 Console.WriteLine($"{coffee.GetDescription()} {coffee.GetCost()}");
@@ -15,4 +16,8 @@ coffee = new ChocolateCrumbs(coffee, 20);
 coffee = new ChocolatePieces(coffee, 4);
 coffee = new Cream(coffee);
 coffee = new Liquor(coffee, LiquorType.Chocolate);
+Console.WriteLine($"{coffee.GetDescription()} {coffee.GetCost()}");
+
+coffee = new Tea(TeaType.Bergamot);
+coffee = new Lemon(coffee, 150);
 Console.WriteLine($"{coffee.GetDescription()} {coffee.GetCost()}");
