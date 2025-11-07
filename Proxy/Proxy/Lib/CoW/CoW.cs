a1639d3ef0873a1970f68da1db3d5067d98c96ad
jaskuen <tankionlinebotserver@mail.ru>
2025-11-07 10:19:18 +0300

    init

---
+++ Proxy/Proxy/Lib/CoW/CoW.cs
@@ -0,0 +1,41 @@
+﻿namespace Proxy.Lib.CoW;
+
+public class CoW<T>
+    where T : class
+{
+    private SharedPtr<T> _obj;
+
+    public CoW(SharedPtr<T> obj)
+    {
+        _obj = obj;
+        _obj.IncrementUsageCount();
+    }
+
+    private CoW(CoW<T> other)
+    {
+        if (other == null) throw new ArgumentNullException(nameof(other));
+        _obj = other._obj;
+        _obj.IncrementUsageCount();
+    }
+
+    public CoW<T> Clone()
+    {
+        return new CoW<T>(this);
+    }
+
+    public virtual T Value
+    {
+        get => _obj.Get();
+        set
+        {
+            if (_obj.GetCount() > 1 && !EqualityExtensions.Equals(value, _obj.Get()))
+            {
+                _obj = new SharedPtr<T>(value);
+            }
+            else
+            {
+                _obj.Set(value);
+            }
+        }
+    }
+}
---
+++ Proxy/Proxy/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Proxy.Lib.CoW;
+
+CoW<IList<int>> list1 = new CoW<IList<int>>(new SharedPtr<IList<int>>(new List<int>() { 1, 2, 3 }));
+CoW<IList<int>> list2 = list1.Clone();
+CoW<IList<int>> list3 = list2.Clone();
+
+Console.WriteLine(list1.Value.Count);
+Console.WriteLine(list2.Value.Count);
+
+list2.Value = [1, 2, 3, 4];
+
+Console.WriteLine(list1.Value.Count);
+Console.WriteLine(list2.Value.Count);
+
+list3.Value = [1, 2, 3];
